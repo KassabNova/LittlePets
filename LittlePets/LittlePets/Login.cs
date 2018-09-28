7890bb52bc900d7d534c54c35cb830c6bcca8118
Carlos Alberto Kassab Andre <3236558@uag.mx>
2018-09-28 18:03:33 -0500

    Login/Menu Forms

--- LittlePets/LittlePets/Login.cs
+++ LittlePets/LittlePets/Login.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace LittlePets
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
